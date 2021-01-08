16d1f146f4037993ca5a4122b7c5202ba538bbff
Dustin Do <dustin.do95@gmail.com>
2021-01-08 14:03:59 +0700

    Update to get ready for deployment

--- packages/server/src/core/app/app.module.ts
+++ packages/server/src/core/app/app.module.ts
@@ -3,11 +3,17 @@ import { config } from 'core'
 import { TypegooseModule } from 'nestjs-typegoose'
 import { GraphQLModule } from '@nestjs/graphql'
 import { UsersModule } from 'modules/users/users.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 const appModules = [UsersModule]
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../../../web', 'build'),
+    }),
+
     TypegooseModule.forRoot(config.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
